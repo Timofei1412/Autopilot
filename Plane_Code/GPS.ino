2c9b75c62c0c34af3ecb935704ee83390d8f2a93
Timofei1412 <ta14122008@gmail.com>
2023-09-01 20:29:28 +0300

    GPS GetDistToLine addition

--- Plane_Code/GPS.ino
+++ Plane_Code/GPS.ino
@@ -1,5 +1,26 @@
-/*
 
 
 
-*/
+
+
+
+
+
+
+
+
+
+
+float RadialX = 0, RadialY = 0, hyp = 0;
+
+float getDistToLine(int R, line Line, plane Coords, int i) {
+  RadialX = Coords.x + (cos(i) * R);
+  RadialY = Coords.Y + (sin(i) * R);
+
+  X_on_Line = (RadialY / Line.k) - Line.b;
+  Y_on_Line = Line.k * RadialX + Line.b;
+
+  hyp = hypot(X_on_Line, Y_on_Line);
+  C = (X_on_Line + Y_on_Line) / min(X_on_Line, Y_on_Line);
+  return hypot(hyt / C, min(X_on_Line, Y_on_Line));
+}
---
+++ Plane_Code/Structs.ino
@@ -0,0 +1,21 @@
+struct point {
+  float x;
+  float y;
+};
+
+struct plane {
+  float x;
+  float y;
+  float heading;
+  float altitude;
+  float speed;
+};
+
+struct line {
+  float k;
+  float b;
+  float x1;
+  float y1;
+  float x2;
+  float y2;
+};
